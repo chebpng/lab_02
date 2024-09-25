13be426da6bdb00489d0daefc05cc7b24a23124a
chebpng <yota3301@gmail.com>
2024-09-25 21:06:12 +0700

    1

--- sort1.pas
+++ sort1.pas
@@ -1,25 +1,27 @@
 ﻿program sort;
 var
-	mass: array[1..100] of integer;
-	a, i, n: integer;
+	mass: array of integer;
+	i, n: integer;
 begin
 	readln(n);
 	
-	if (n < 1) or (n > 100) then
-	begin
-		writeln('Длина массива должна быть в пределах 0 > x > 100.');
-		readln();
-		exit;
-	end;
+	if (n < 1) or (n > 1000000) then
+	  begin
+		  writeln('Длина массива должна быть в пределах 0 > x > 1000000.');
+		  readln();
+		  exit;
+	  end;
+
+SetLength(mass, n);
 	
 	randomize;
-	for i := 1 to n do
+	for i := 0 to n-1 do
 	begin
 		mass[i] := Random(2147483646);
 	end;
 	
 	writeln();
-	for i := 1 to n do
+	for i := 0 to n-1 do
 	begin
 		write(mass[i], ' ');
 	end;

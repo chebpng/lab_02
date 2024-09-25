8627933037ae38e6ae5c8a1aaea5617dcc7e04e0
chebpng <yota3301@gmail.com>
2024-09-25 20:53:03 +0700

    Заполнение массива

---
+++ sort1.pas
@@ -0,0 +1,26 @@
+﻿program sort;
+var
+	mass: array[1..100] of integer;
+	a, i, n: integer;
+begin
+	readln(n);
+	
+	if (n < 1) or (n > 100) then
+	begin
+		writeln('Длина массива должна быть в пределах 0 > x > 100.');
+		readln();
+		exit;
+	end;
+	
+	randomize;
+	for i := 1 to n do
+	begin
+		mass[i] := Random(2147483646);
+	end;
+	
+	writeln();
+	for i := 1 to n do
+	begin
+		write(mass[i], ' ');
+	end;
+end.

45d67dd14a9360b3d07b57b657a39ea4d8c2f009
chebpng <yota3301@gmail.com>
2024-09-26 17:26:55 +0700

    1 метод сортировки

--- sort1.pas
+++ sort1.pas
@@ -1,29 +1,61 @@
 ﻿program sort;
 var
-	mass: array of integer;
-	i, n: integer;
+  mass: array of integer;
+  i, n, a, IsSort: integer;
 begin
-	readln(n);
-	
-	if (n < 1) or (n > 1000000) then
-	  begin
-		  writeln('Длина массива должна быть в пределах 0 > x > 1000000.');
-		  readln();
-		  exit;
-	  end;
+  readln(n);
 
-SetLength(mass, n);
-	
-	randomize;
-	for i := 0 to n-1 do
-	begin
-		mass[i] := Random(2147483646);
-	end;
-	
-	writeln();
-	for i := 0 to n-1 do
-	begin
-		write(mass[i], ' ');
-	end;
+  if (n < 1) or (n > 1000000) then
+  begin
+    writeln('Длина массива должна быть в пределах 0 > x > 1000000.');
+    readln();
+    exit;
+  end;
+
+  SetLength(mass, n);
+
+  randomize;
+  for i := 0 to n-1 do
+  begin
+    if random(2) = 0 then
+      mass[i] := Random(999)
+    else
+      mass[i] := -1*(Random(999))
+  end;
+  
+
+  writeln('------------------------------неотсортированный массив---------------------------');
+  writeln();
+  for i := 0 to n-1 do
+  begin
+    write(mass[i], ' ');
+  end;
+
+
+  IsSort := 0;
+  while IsSort <> 1 do
+  begin
+    IsSort := 1; // Предполагаем, что массив отсортирован
+    for i := 0 to n-2 do
+    begin
+      if mass[i] > mass[i+1] then
+      begin
+        a := mass[i];
+        mass[i] := mass[i+1];
+        mass[i+1] := a;
+        IsSort := 0; // Массив не отсортирован, устанавливаем IsSort в 0
+        break; // Прерываем цикл, так как нужно сделать еще один проход
+      end;
+    end;
+  end;
+
+  writeln();
+  writeln('------------------------------отсортированный массив---------------------------');
+  writeln();
+  for i := 0 to n-1 do
+  begin
+    write(mass[i], ' ');
+  end;
+
+  readln();
 end.
-//1

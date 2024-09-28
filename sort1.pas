ec5a5e1efe05a220a02d219b9e2d8c36aee5ec7e
chebpng <yota3301@gmail.com>
2024-09-28 09:34:33 +0700

    2 сортировка рабочая

--- sort1.pas
+++ sort1.pas
@@ -1,7 +1,7 @@
 ﻿program sort;
 var
   mass, massSort: array of integer;
-  i, ii, n, a, IsSort, SortType, max, maxI: integer;
+  i, n, a, Sorted, SortType, temp: integer;
 begin
   writeln('Выберите тип сортировки');
   writeln('1- Простейший алгоритм сортировки');
@@ -45,10 +45,10 @@ begin
 //первый метод сортировки---------------------------------------
 if SortType = 1 then
   begin  
-    IsSort := 0;
-    while IsSort <> 1 do
+    Sorted := 0;
+    while Sorted <> 1 do
     begin
-      IsSort := 1; // Предполагаем, что массив отсортирован
+      Sorted := 1; // Предполагаем, что массив отсортирован
       for i := 0 to n-2 do
       begin
         if mass[i] > mass[i+1] then
@@ -56,21 +56,11 @@ if SortType = 1 then
           a := mass[i];
           mass[i] := mass[i+1];
           mass[i+1] := a;
-          IsSort := 0; // Массив не отсортирован, устанавливаем IsSort в 0
+          Sorted := 0; // Массив не отсортирован, устанавливаем IsSort в 0
           break; // Прерываем цикл, так как нужно сделать еще один проход
         end;
        end;
       end;
-      
-      //вывод итога сортировки
-  writeln();
-  writeln('------------------------------отсортированный массив---------------------------');
-  writeln();
-  for i := 0 to n-1 do
-  begin
-    write(mass[i], ' ');
-  end;
-      
     end;
 //первый метод сортировки---------------------------------------   
  
@@ -78,40 +68,36 @@ if SortType = 1 then
 
 if SortType = 2 then
   begin
-    //I.5. Идея алгоритма упорядочения выбором
-//Основа – поиск максимального значения в массиве. Элемент с максимальным значением меняется местами с последним элементом массива. 
-//Затем поиск максимума проводится среди первых (n-1) элементов. Найденный элемент меняется местами с предпоследним, и т.д., 
-//пока область поиска максимума не сократится до одного элемента.
+
 SetLength(massSort, n);
+n := n - 1;
 
-for ii := 0 to n-1 do
-begin
-  max := -1000; 
-  maxI := 0; 
-  for i := 1 to n-1 do
+while Sorted <> 1 do
   begin
-    if max < mass[i] then
-    begin
-      max := mass[i];
-      maxI := i;
-    end;
-  end;
-  massSort[ii] := max;
-  mass[maxI] := mass[ii]; 
-  mass[maxI] := -1000; // Удаляем максимальный элемент из mass
-end;
+    Sorted := 1; 
 
-//вывод итога сортировки
+    for i := 0 to n - 2 do 
+      begin
+        if mass[i] > mass[i+1] then 
+          begin
+            Sorted := 0; 
+            temp := mass[i];
+            mass[i] := mass[i+1];
+            mass[i+1] := temp; 
+          end;
+      end;
+  end;
+  end;
+//второй метод сортировки--------------------------------------- 
+
+
+  //вывод итога сортировки
   writeln();
   writeln('------------------------------отсортированный массив---------------------------');
   writeln();
   for i := 0 to n-1 do
   begin
-    write(massSort[i], ' ');
-  end;
-
-  end;
-//второй метод сортировки--------------------------------------- 
-    
-
+    write(mass[i], ' ');
+  end; 
+//1
 end.
